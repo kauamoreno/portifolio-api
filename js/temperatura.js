77abcdc9a7d979b07c8526029e37b8e2591621b2
Aluno <kaua.moreno2005@gmail.com>
2023-02-13 10:46:15 -0300

    update style.css

--- js/temperatura.js
+++ js/temperatura.js
@@ -17,7 +17,6 @@ function pesquisar() {
         //const response = await axios.get('https://api.openweathermap.org/data/2.5/weather?q=Mogi%20das%20Cruzes&units=metric&appid=64ed82577ced7f69cb1687f0ce536131')
         const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=64ed82577ced7f69cb1687f0ce536131`);
 
-
         //Resposta em (Celsius e localização)
         const tempCelsius = response.data.main.temp;
         document.querySelector("#temperatura").innerHTML = `A temperatura atual de ${cidade} é de ${tempCelsius.toFixed(2)} °C`
