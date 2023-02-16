0aec6a35ebf076bd2434f9b1d3c3e4b66aa4aac2
Aluno <kaua.moreno2005@gmail.com>
2023-02-16 10:52:21 -0300

    corrigindo ecommerce.html

--- js/ecommerce.js
+++ js/ecommerce.js
@@ -6,9 +6,9 @@ fetch("../json/ecommerce.json")
     .then(corpo => {
 
         //Acessando os dados do objeto json e adicionando ao HTML 
-        document.getElementById("imagem").innerHTML = corpo.imagem;
-        document.getElementById("nome").innerHTML = corpo.name;
-        document.getElementById("disco").innerHTML = corpo.albun;
-        document.getElementById("estilo").innerHTML = corpo.style;
-        document.getElementById("preco").innerHTML = corpo.price;
+        document.getElementById("imagem").innerHTML = corpo.nirvana.imagem;
+        document.getElementById("nome").innerHTML = corpo.nirvana.name;
+        document.getElementById("disco").innerHTML = corpo.nirvana.albun;
+        document.getElementById("estilo").innerHTML = corpo.nirvana.style;
+        document.getElementById("preco").innerHTML = corpo.nirvana.price;
     })
