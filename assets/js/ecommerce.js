2ea86521b02821ad7d6ec9afc14c0b9bc7d1faea
Kauã Moreno <kaua.moreno2005@gmail.com>
2023-02-25 13:20:02 -0300

    transição de css para scss

--- assets/js/ecommerce.js
+++ assets/js/ecommerce.js
@@ -1,14 +1,32 @@
 //Metodo fetch() para buscar o arquivo dados.json, ou qualquer API
 //Transformando o json eum um objeto usando o metodo response.json
 
-fetch("../json/ecommerce.json")
+fetch("assets/json/dadosEcommerce.json")
     .then(response => response.json())
     .then(corpo => {
 
-        //Acessando os dados do objeto json e adicionando ao HTML 
-        document.getElementById("imagem").innerHTML = corpo.nirvana.imagem;
-        document.getElementById("nome").innerHTML = corpo.nirvana.name;
-        document.getElementById("disco").innerHTML = corpo.nirvana.albun;
-        document.getElementById("estilo").innerHTML = corpo.nirvana.style;
-        document.getElementById("preco").innerHTML = corpo.nirvana.price;
+        // Acessando os dados do objeto json e adicionando ao HTML 
+
+        //NIRVANA
+        document.getElementById("imagem-nirvana").innerHTML = corpo.nirvana.imagem;
+        document.getElementById("nome-nirvana").innerHTML += corpo.nirvana.name;
+        document.getElementById("disco-nirvana").innerHTML += corpo.nirvana.album;
+        document.getElementById("estilo-nirvana").innerHTML += corpo.nirvana.style;
+        document.getElementById("preco-nirvana").innerHTML += corpo.nirvana.price;
+
+        //QUEEN
+        document.getElementById("imagem-queen").innerHTML = corpo.queen.imagem;
+        document.getElementById("nome-queen").innerHTML += corpo.queen.name;
+        document.getElementById("disco-queen").innerHTML += corpo.queen.album;
+        document.getElementById("estilo-queen").innerHTML += corpo.queen.style;
+        document.getElementById("preco-queen").innerHTML += corpo.queen.price;
+
+        //GUNS
+        document.getElementById("imagem-guns").innerHTML = corpo.guns.imagem;
+        document.getElementById("nome-guns").innerHTML += corpo.guns.name;
+        document.getElementById("disco-guns").innerHTML += corpo.guns.album;
+        document.getElementById("estilo-guns").innerHTML += corpo.guns.style;
+        document.getElementById("preco-guns").innerHTML += corpo.guns.price;
+
     })
+    .catch(error => console.log("nao encontrado"));
