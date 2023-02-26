221421cccd9e66f0fc66500249439608e4d4dce4
Kauã Moreno <kaua.moreno2005@gmail.com>
2023-02-26 01:11:53 -0300

    add pagina movie

---
+++ assets/js/movie.js
@@ -0,0 +1,35 @@
+function pesquisa() {
+    const apiKey = '27f84c68';
+    const filme = document.getElementById('filme').value;
+
+    //http://www.omdbapi.com/?i=tt3896198&apikey=${apiKey}
+
+    fetch(`http://www.omdbapi.com/?t=${filme}&apikey=${apiKey}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+
+            document.getElementById('titulo').innerHTML = data.Title
+            document.getElementById('poster').src = data.Poster
+            document.getElementById('resumo').innerHTML = `<b>Resumo: </b> ${data.Plot}`
+            document.getElementById('lancamento').innerHTML = `<b>Lançamento: </b> ${data.Released}`
+            document.getElementById('genero').innerHTML = `<b>Genero: </b> ${data.Genre}`
+            document.getElementById('duracao').innerHTML = `<b>Duração: </b> ${data.Runtime}`
+            document.getElementById('rating').innerHTML = `<b>Avaliação: </b> ${data.imdbRating}`
+            document.getElementById('votos').innerHTML = `<b>Votos: </b> ${data.imdbVotes}`
+        })
+        .catch(error => {
+            Swal.fire({
+                icon: 'error',
+                title: "Filme não reconhecido",
+                text: 'Insira um filme existente!',
+            })
+        });
+
+    document.getElementById('display').style.display = 'block';
+}
+
+
+
+
+
