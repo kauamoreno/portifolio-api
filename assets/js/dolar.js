fcbb161d56ddcb8a9411bb10629c101ec545de7f
Kauã Moreno <kaua.moreno2005@gmail.com>
2023-02-27 20:03:01 -0300

    responsividade dolar.html

--- assets/js/dolar.js
+++ assets/js/dolar.js
@@ -1,6 +1,8 @@
 fetch(`https://economia.awesomeapi.com.br/json/last/USD-BRL`).then(resposta => {
     return resposta.json(); 
 }).then(economia => {
+    var dolar = economia.USDBRL.bid;
     console.log(economia);
-    document.getElementById("dolarAgora").innerHTML = economia.USDBRL.code + " " + economia.USDBRL.bid;
+
+    document.getElementById("dolarAgora").innerHTML = economia.USDBRL.code + " " + dolar;
 })
