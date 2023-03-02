98347bb7f7d6cfd8893e0f766f439016efb32301
Aluno <kaua.moreno2005@gmail.com>
2023-03-02 11:05:00 -0300

    tratamento de erro movie input

--- assets/js/movie.js
+++ assets/js/movie.js
@@ -9,27 +9,24 @@ function pesquisa() {
         .then(data => {
             console.log(data);
 
-            document.getElementById('titulo').innerHTML = data.Title
-            document.getElementById('poster').src = data.Poster
-            document.getElementById('resumo').innerHTML = `<b>Resumo: </b> ${data.Plot}`
-            document.getElementById('lancamento').innerHTML = `<b>Lançamento: </b> ${data.Released}`
-            document.getElementById('genero').innerHTML = `<b>Genero: </b> ${data.Genre}`
-            document.getElementById('duracao').innerHTML = `<b>Duração: </b> ${data.Runtime}`
-            document.getElementById('rating').innerHTML = `<b>Avaliação: </b> ${data.imdbRating}`
-            document.getElementById('votos').innerHTML = `<b>Votos: </b> ${data.imdbVotes}`
+            if (data.Response == 'True') {
+                document.getElementById('titulo').innerHTML = data.Title
+                document.getElementById('poster').src = data.Poster
+                document.getElementById('resumo').innerHTML = `<b>Resumo: </b> ${data.Plot}`
+                document.getElementById('lancamento').innerHTML = `<b>Lançamento: </b> ${data.Released}`
+                document.getElementById('genero').innerHTML = `<b>Genero: </b> ${data.Genre}`
+                document.getElementById('duracao').innerHTML = `<b>Duração: </b> ${data.Runtime}`
+                document.getElementById('rating').innerHTML = `<b>Avaliação: </b> ${data.imdbRating}`
+                document.getElementById('votos').innerHTML = `<b>Votos: </b> ${data.imdbVotes}`
+
+                document.getElementById('display').style.display = 'block';
+            } else {
+                document.getElementById('display').style.display = 'none';
+                Swal.fire({
+                    icon: 'error',
+                    title: "Filme não reconhecido",
+                    text: 'Insira um filme existente!',
+                })
+            }
         })
-        .catch(error => {
-            Swal.fire({
-                icon: 'error',
-                title: "Filme não reconhecido",
-                text: 'Insira um filme existente!',
-            })
-        });
-
-    document.getElementById('display').style.display = 'block';
 }
-
-
-
-
-
