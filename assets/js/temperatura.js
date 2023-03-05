2beffc9a24027a92e6f536fd2a7627671ad8ed53
Kauã Moreno <kaua.moreno2005@gmail.com>
2023-03-05 17:39:17 -0300

    try/catch temperatura input

--- assets/js/temperatura.js
+++ assets/js/temperatura.js
@@ -14,22 +14,30 @@ function pesquisar() {
         //Fazendo uma chamada à API usando a biblioteca AXIOS. A URL da API inclui as informações da localidade (Mogi das Cruzes)
         //A unidade de medida (Celsius) e a chave API KEY
 
-        //const response = await axios.get('https://api.openweathermap.org/data/2.5/weather?q=Mogi%20das%20Cruzes&units=metric&appid=64ed82577ced7f69cb1687f0ce536131')
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=64ed82577ced7f69cb1687f0ce536131`);
+        try {
+            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=64ed82577ced7f69cb1687f0ce536131`);
 
-        //Resposta em (Celsius e localização)
-        const tempCelsius = response.data.main.temp;
+            //Resposta em (Celsius e localização)
+            const tempCelsius = response.data.main.temp;
 
-        const iconCode = response.data.weather[0].icon;
-        const clima = response.data.weather[0].main;
-        const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
-        document.getElementById('icon').src = iconUrl
+            const iconCode = response.data.weather[0].icon;
+            const clima = response.data.weather[0].main;
+            const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
+            document.getElementById('icon').src = iconUrl
 
-        document.getElementById('cidateTitulo').innerHTML = cidade
-        document.querySelector("#temperatura").innerHTML = `${tempCelsius.toFixed(2)} °C`
-        document.getElementById('clima').innerHTML = `Clima: ${clima}`;
+            document.getElementById('cidateTitulo').innerHTML = cidade
+            document.querySelector("#temperatura").innerHTML = `${tempCelsius.toFixed(2)} °C`
+            document.getElementById('clima').innerHTML = `Clima: ${clima}`;
 
-        console.log(response);
+            console.log(response);
+        } catch {
+            Swal.fire({
+                icon: 'error',
+                title: "Local não reconhecido",
+                text: 'Insira um local existente!',
+            })
+        }
+
 
     }
 
